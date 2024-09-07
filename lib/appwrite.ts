0a32af64d31552af3f4ae24e74431426ee6799f6
delabahmani <delabahmani@gmail.com>
2024-09-06 23:20:51 -0400

    Sign up and Sign in pages implemented, onto the logic now

---
+++ lib/appwrite.ts
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.jsm.bummd",
+  projectId: "66dbc0090031509099d3",
+  databaseId: "66dbc1860013322d9342",
+  userCollectionId: "66dbc1a1000033c401a9",
+  videoCollectionId: "66dbc320003709fb6657",
+  storageId: "66dbc45d0006620f49d8",
+};
